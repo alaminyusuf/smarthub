1d20a176bfad54958f5637541092f45415b893ad
Al-amin Yusuf <alaminyusuf131@gmail.com>
2021-10-18 15:22:37 +0100

    set apollo server sessions and db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,82 @@
 import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { MyContext } from "./types";
+
 import express from "express";
 import dotenv from "dotenv";
-// import { createConnection } from "typeorm";
+import connectRedis from "connect-redis";
+import Redis from "ioredis";
+import session from "express-session";
+import cors from "cors";
 
-dotenv.config({});
-// const MONGO_URI = process.env.MONGO_URI;
-// const DB_NAME = process.env.DB_NAME;
+import { UserResolver } from "./graphql/Mutation/user";
+import { UserQuery } from "./graphql/Query/user";
 
-const main = async () => {
+dotenv.config();
+
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
+
+async function StartServer() {
+	await createConnection({
+		type: "mongodb",
+		host: "localhost",
+		port: 27017,
+		database: "smarthub",
+		entities: ["src/models/*{.ts,.js}"],
+		useUnifiedTopology: true,
+	}).then((conn) => console.log("MongoDB connected", conn.isConnected));
+
 	const app = express();
-	// await createConnection({
-	// 	type: "mongodb",
-	// 	url: MONGO_URI,
-	// 	database: DB_NAME,
-	// 	useNewUrlParser: true,
-	// 	useUnifiedTopology: true,
-	// }).then(() => console.info("MongoDB connected"));
 
-	app.listen(4100, () => console.info("app listening on PORT 4100"));
-};
+	app.use(
+		cors({
+			origin: "http://localhost:3000",
+			credentials: true,
+		})
+	);
 
-main();
+	const RedisStore = connectRedis(session);
+	const redis = new Redis({
+		host: "localhost",
+		port: 6379,
+	});
+
+	app.use(
+		session({
+			store: new RedisStore({
+				client: redis,
+				disableTTL: true,
+				disableTouch: true,
+			}),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 5,
+				httpOnly: true,
+				secure: false,
+			},
+			secret: COOKIE_SECRET || "secret",
+			resave: false,
+			name: "smarthub",
+			saveUninitialized: false,
+		})
+	);
+
+	app.get("/", (req, res) =>
+		res.send("<h1>Hello from this awesome mock Api</h1>")
+	);
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [UserResolver, UserQuery],
+			validate: false,
+		}),
+		context: ({ req, res }): MyContext => ({ req, res, redis }),
+	});
+
+	apolloServer.applyMiddleware({ app, cors: false, path: "/graphql" });
+
+	app.listen(4100, () => console.log("Server running"));
+}
+
+StartServer().catch((error) => console.error("Error:", error));
---
+++ server/src/types.ts
@@ -0,0 +1,16 @@
+import { ObjectID } from "typeorm";
+import { Redis } from "ioredis";
+import { Request, Response } from "express";
+import { Session } from "express-session";
+
+export type MyContext = {
+	req: Request & { session: Session };
+	redis: Redis;
+	res: Response;
+};
+
+declare module "express-session" {
+	interface SessionData {
+		userId: ObjectID;
+	}
+}
